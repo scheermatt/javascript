394e86532a1957bd910ac79f27c692cbb0932068
Matheus Scheer <mathscheer@gmail.com>
2025-05-10 17:01:38 -0300

    Update index.js

--- exercicios/aula3/index.js
+++ exercicios/aula3/index.js
@@ -112,4 +112,25 @@ for (let chave in p1) {
 }
 */
 
+const pessoas = [
+    { id: 3, nome: 'Matheus'},
+    { id: 2, nome: 'Kadu' },
+    { id: 1, nome: 'Nikao' }
+];
+/*
+const novasPessoas = {};
+for (const pessoa of pessoas) {
+    const { id } = pessoa;
+    novasPessoas[id] = { ...pessoa };
+}
+*/
 
+const novasPessoas = new Map();
+for (const pessoa of pessoas) {
+    const { id } = pessoa;
+    novasPessoas.set(id, { ...pessoa });
+}
+
+for (const pessoas of novasPessoas) { 
+    console.log(pessoas);
+ }
